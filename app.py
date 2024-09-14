16eff02dd64cf30cef667fbf68d6097057790d3c
Velicanstvena <66689566+Velicanstvena@users.noreply.github.com>
2024-09-14 20:45:06 +0200

    Added postgres

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate, migrate
 from sqlalchemy import values
 
+from dotenv import load_dotenv
+
 from db import db
 from blocklist import BLOCKLIST
 import models
@@ -18,6 +20,8 @@ from resources.user import blp as UserBlueprint
 
 def create_app(db_url=None):
     app = Flask(__name__)
+    load_dotenv()
+
     app.config["PROPAGATE_EXCEPTIONS"] = True
     app.config["API_TITLE"] = "Stores REST API"
     app.config["API_VERSION"] = "v1"
